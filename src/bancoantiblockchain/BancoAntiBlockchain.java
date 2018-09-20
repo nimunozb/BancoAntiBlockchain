c253ddf693342c8016ed359e18f70edbf504a442
Horacio <Horacio@aguacristal>
2018-09-20 12:25:41 -0500

    prueba12

--- src/bancoantiblockchain/BancoAntiBlockchain.java
+++ src/bancoantiblockchain/BancoAntiBlockchain.java
@@ -10,25 +10,43 @@ package bancoantiblockchain;
  * @author Horacio
  */
 public class BancoAntiBlockchain {
-    Cuenta cuenta=new Cuenta();
+    private static Cuenta cuenta=new Cuenta();
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+    
+        Cuenta cuenta1=new Cuenta(1,1,1,1006767,1400,"elver Galarga");   
+         Cuenta cuenta2=new Cuenta(2,7,4,100678,800,"Rosa Melcalvo");
+         
+        
+      cuenta1.getMovimiento().RetirarCuenta(cuenta1.getSaldo(), 350);
+          cuenta1.getMovimiento().RetirarCuenta(cuenta1.getSaldo(), 380);
+           
+        
+        /*    cuenta2.movimiento.consignarCuenta(cuenta2.saldo, 50);
+                cuenta2.movimiento.consignarCuenta(cuenta2.saldo, 50);*/
+                
+                      
+         listadoMovimientos(cuenta1);
+         //listadoMovimientos(cuenta2);
+    
+    
+    
     }
     
-    public static void listadoMovimientos(){
-   /*   for (int i = 0; i <40; i++) {
-          if (Cuenta().movimientosRealizados[i]!=null){
-              System.out.println("este movimiento sde raelizo el dia "+movimientosRealizados[i].metadato.getDia()
-              +"el mes "movimientosRealizados[i].metadato.getMes()+"el año "+movimientosRealizados[i].metadato.getAno()+
-                      "codigo de la trasaccion "+movimientosRealizados[i].transaccion.getCodigo()+"por un valor de "+
-                         movimientosRealizados[i].valorCantidad);
+    public static void listadoMovimientos(Cuenta cuenta){
+     for (int i = 0; i <40; i++) {
+          if (cuenta.getMovimiento().getMovimientosRealizados()[i]!=null){
+              System.out.println("este movimiento se raelizo el dia "+cuenta.getMovimiento().getMovimientosRealizados()[i].metadato.getDia()
+              +" el mes "+cuenta.getMovimiento().getMovimientosRealizados()[i].metadato.getMes()+" el año "+cuenta.getMovimiento().getMovimientosRealizados()[i].metadato.getAno()+
+                      " ,codigo de la trasaccion "+cuenta.getMovimiento().getMovimientosRealizados()[i].transaccion.getCodigo()+" por un valor de "+
+                         cuenta.getMovimiento().getMovimientosRealizados()[i].valorCantidad);
           }
       }
       
-      */
+      
   }
     
     
